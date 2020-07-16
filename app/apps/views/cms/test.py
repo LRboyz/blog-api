145b8f9fb00a924bc337130c56ec8a04e1eaa474
刘锐 <liurui@LRboydeMac.local>
2020-07-16 22:19:43 +0800

    添加测试接口

--- app/apps/views/cms/test.py
+++ app/apps/views/cms/test.py
@@ -1,19 +1,13 @@
 # -*- coding:utf-8 -*-
 
 from flask import Blueprint, jsonify
-from hashlib import md5
-from apps.core.token_auth import admin_required
-from apps.validaters.forms import RegisterForm
 
 test = Blueprint('test', __name__)
 
 
 @test.route('')
-# @admin_required
 def test_api():
-    a = 'https://www.gravatar.com/avatar/' + md5(b'603552916@qq.com').hexdigest()
-    print(a)
-    return jsonify(code=201, url=a)
+    return jsonify(code=200, msg='test API is Success!')
 
 
 
